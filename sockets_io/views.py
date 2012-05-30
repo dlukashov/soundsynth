0f7fa5744cf883c848741cc6c22f779136aa99f3
Dimitry Lukashov <d.lukashov@gmail.com>
2012-05-29 21:26:58 -0400

    actually added socketio as an app

---
+++ sockets_io/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+
+buffer = []
+
+def sockectio(request):
+    socketio = request.environ['socketio']
+    if socketio.on_connect():
+        socketio.send({'buffer': buffer})
+        socketio.broadcast({'announcement': socketio.session.session_id + ' connected'})
+
+    while True:
+        message = socketio.recv()
+
+        if len(message) == 1:
+            message = message[0]
+            message = {'message': [socketio.session.session_id, message]}
+            buffer.append(message)
+            if len(buffer) > 15:
+                del buffer[0]
+            socketio.broadcast(message)
+        else:
+            if not socketio.connected():
+                socketio.broadcast({'announcement': socketio.session.session_id + ' disconnected'})
+                break
+
+    return HttpResponse()                                                                                                                           return HttpResponse()uu
